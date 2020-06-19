2732c5836a81fce43b00b79e5c1721d97bd9b0ca
Matthew <2565361+mshafer1@users.noreply.github.com>
2020-06-19 15:45:13 -0500

    setup testing

---
+++ __tests__/test_parsing_config.py
@@ -0,0 +1,53 @@
+import pytest
+
+basic_url = "/?length=1&comment0=test+comment&func0=KEY&skey0%5B%5D=CTRL&skey0%5B%5D=ALT&skeyValue0=i&Window0=ahk_exe+chrome.exe&Program0=chrome.exe&option0=ActivateOrOpen"
+basic_hotstring_url = "/?indexes=0&comment0=&func0=STRING&skeyValue0=btw&input0=by+the+way&option0=Replace"
+
+@pytest.mark.parametrize(
+    "expected_hotkey_row_ids,url",
+    (
+        (["0"], ""),
+        (["0"], basic_url),
+        (
+            ["0"],
+            "/?length=1&comment1=test+comment&func1=KEY&skey0%5B%5D=CTRL&skey0%5B%5D=ALT&skeyValue1=i&Window1=ahk_exe+chrome.exe&Program1=chrome.exe&option1=ActivateOrOpen",
+        ),  # down packs numbers
+    ),
+)
+def test__url__load_page__result_has_expected_hotkey_row_ids(
+    url, expected_hotkey_row_ids, browser, parser, base_url
+):
+    browser.get(base_url + url)
+    page = browser.page_source
+
+    parsed = parser(page)
+    row_id_inputs = parsed.find_all("input", {"class": "js-index"})
+
+    assert [id_input["value"] for id_input in row_id_inputs] == expected_hotkey_row_ids
+
+
+@pytest.mark.parametrize(
+    "expected_trigger_types,ids,url", (
+        (["STRING"], ["0"], basic_hotstring_url),
+        (["KEY"], ["0"], "/"),
+        (["KEY"], ["0"], basic_url),
+    )
+)
+def test__url_and_expected_trigger_types__load_page__assert_has_expected_trigger_types(
+    expected_trigger_types, url, browser, parser, base_url, ids
+):
+    browser.get(base_url + url)
+    page = browser.page_source
+
+    parsed = parser(page)
+    row_trigger_types = parsed.find_all("input", {"type": "radio"})
+    row_trigger_types = [
+        row_trigger_type for row_trigger_type in row_trigger_types
+        if row_trigger_type["name"].startswith("func")
+    ]
+    row_trigger_types = [
+        row_trigger_type for row_trigger_type in row_trigger_types
+        if row_trigger_type.has_attr('checked')
+    ]
+
+    assert [trigger['value'] for trigger in row_trigger_types] == expected_trigger_types
---
+++ conftest.py
@@ -0,0 +1,63 @@
+import pytest
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--driver-path",
+        dest="driver_path",
+        action="store",
+        help="Path to the chrome webdriver to use",
+        required=True,
+    )
+
+
+def pytest_generate_tests(metafunc):
+    # This is called for every test. Only get/set command line arguments
+    # if the argument is specified in the list of test "fixturenames".
+    driver_path = metafunc.config.getoption("driver_path", None)
+
+    if driver_path is None:
+        raise Exception("Must provide --driver_path")
+
+    if "driver_path" in metafunc.fixturenames:
+        metafunc.parametrize("driver_path", [metafunc.config.getoption("driver_path")])
+
+
+@pytest.fixture()
+def browser(driver_path):
+    if not browser.result:
+        opts = Options()
+        # opts.add_argument("--headless")
+        # opts.add_argument("--disable-gpu")  # Last I checked this was necessary.
+
+        browser.result = webdriver.Chrome(driver_path, options=opts)
+    yield browser.result
+
+
+browser.result = None
+
+
+@pytest.fixture()
+def root_page(browser, base_url):
+    if not root_page.result:
+        browser.get(base_url)
+        root_page.result = browser
+    yield root_page.result
+
+
+root_page.result = None
+
+
+@pytest.fixture()
+def parser():
+    def _get_parser(html):
+        return BeautifulSoup(html, "html.parser")
+
+    yield _get_parser
+
+@pytest.fixture()
+def base_url():
+    return "http://localhost:4000"
