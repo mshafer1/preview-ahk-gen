63ec93f7669d2463d2602091d446e909ae0cec77
Matthew Shafer (laptop) <mshafer1@harding.edu>
2020-06-25 21:25:41 -0500

    starting on it

--- __tests__/_utils.py
+++ __tests__/_utils.py
@@ -1,26 +1,35 @@
+from collections import defaultdict
+from enum import Enum
+
 import pytest
 from selenium.webdriver.common.by import By
 
-from enum import Enum
 
 class TriggerTypes(Enum):
-    KEY = 'KEY'
-    STRING = 'STRING'
+    KEY = "KEY"
+    STRING = "STRING"
 
-class AssertionObject():
+
+class AssertionObject:
     def __init__(self, expected_trigger_types=[], expected_hotkey_ids=[]):
         self._trigger_types = expected_trigger_types
         self._hotkey_ids = expected_hotkey_ids
-    
+
     def check(self, browser, parser, subtests):
         page = browser.page_source
 
         if self._trigger_types:
-            checked_selectors = browser.find_elements(By.CSS_SELECTOR, "input[type='radio']:checked")
-            values = [selector.get_attribute('value') for selector in checked_selectors]
-            with subtests.test(expected_trigger_types=self._trigger_types, actual_trigger_types=values):
-                assert values == [trigger_type.value for trigger_type in self._trigger_types]
-    
+            checked_selectors = browser.find_elements(
+                By.CSS_SELECTOR, "input[type='radio']:checked"
+            )
+            values = [selector.get_attribute("value") for selector in checked_selectors]
+            with subtests.test(
+                expected_trigger_types=self._trigger_types, actual_trigger_types=values
+            ):
+                assert values == [
+                    trigger_type.value for trigger_type in self._trigger_types
+                ]
+
         if self._hotkey_ids:
             parsed = parser(page)
             row_id_inputs = parsed.find_all("input", {"class": "js-index"})
@@ -28,3 +37,52 @@ class AssertionObject():
 
             with subtests.test(expected_hotkey_ids=self._hotkey_ids, hotkey_ids=values):
                 assert values == self._hotkey_ids
+
+
+def _get_elements_and_desired_value_through_browser(
+    path_type, path, filter, filter_attr, desired_attr, browser
+):
+    elements = browser.find_elements(path_type, path)
+    desired_elements = [i for i in elements if filter(i.get_attribute(filter_attr))]
+    return {
+        i.get_attribute("name"): i.get_attribute(desired_attr) for i in desired_elements
+    }
+
+
+def loaded_data(browser, parser):
+    data = defaultdict(dict)
+
+    page = browser.page_source
+    parsed = parser(page)
+
+    uses_ids = True
+
+    row_id_inputs = parsed.find_all("input", {"class": "js-index"})
+    hotkey_ids = [id_input["value"] for id_input in row_id_inputs]
+    if not hotkey_ids:
+        uses_ids = False
+    # else:
+    #     data["hotkey_ids"] = hotkey_ids
+
+    text_inputs = parsed.find_all("input", {"type": "text"})
+
+    # text_inputs = browser.find_elements(By.CSS_SELECTOR,)
+    # comment_inputs = [
+    #     i for i in text_inputs if i.get_attribute("name").startswith("comment")
+    # ]
+
+    comment_inputs = _get_elements_and_desired_value_through_browser(
+        By.CSS_SELECTOR,
+        "input[type='text']",
+        lambda v: v.startswith("comment"),
+        "name",
+        "value",
+        browser,
+    )
+
+    # comment_inputs = [i for i in text_inputs if i.get("name", "").startswith("comment")]
+    for name, comment in comment_inputs.items():
+        id_value = name[len("comment") :]
+        data[id_value]["comment"] = comment
+
+    return dict(data)
--- __tests__/test_parsing_config.py
+++ __tests__/test_parsing_config.py
@@ -1,11 +1,18 @@
+import json
+import os.path
+from pathlib import Path
+
 import pytest
 
 from _utils import AssertionObject
 from _utils import TriggerTypes
+from _utils import loaded_data
 
 basic_url = "/?length=1&comment0=test+comment&func0=KEY&skey0%5B%5D=CTRL&skey0%5B%5D=ALT&skeyValue0=i&Window0=ahk_exe+chrome.exe&Program0=chrome.exe&option0=ActivateOrOpen"
 basic_hotstring_url = "/?indexes=0&comment0=&func0=STRING&skeyValue0=btw&input0=by+the+way&option0=Replace"
 
+ROOT_TEST_PATH = Path(os.path.realpath(__file__)).parent
+TEST_CASE_DIR = ROOT_TEST_PATH / 'loaded_pages_snapshots'
 
 @pytest.mark.parametrize(
     "url,expected_values", (
@@ -20,3 +27,18 @@ def test__url_and_expected_trigger_types__load_page__assert_has_expected_trigger
     browser.get(base_url + url)
 
     expected_values.check(browser, parser, subtests)
+
+@pytest.mark.parametrize("test_name,url", (
+    ("base", "/"),
+    ("btw__by_the_way", "/?length=1&comment0=&func0=STRING&skeyValue0=btw&input0=by+the+way&option0=Replace"),
+    ("btw__by_the_way_commented", "/?length=1&comment0=btw+%3D+by+the+way&func0=STRING&skeyValue0=btw&input0=by+the+way&option0=Replace"),
+    ("ctrl_alt_i__chrome", "/?length=1&comment0=&func0=KEY&skey0%5B%5D=CTRL&skey0%5B%5D=ALT&skeyValue0=i&Window0=ahk_exe+chrome.exe&Program0=chrome.exe&option0=ActivateOrOpen"),
+
+))
+def test__url__load_page__loaded_data_matches_expected(test_name, url, browser, parser, base_url, snapshot):
+    browser.get(base_url + url)
+
+    data = loaded_data(browser, parser)
+
+    snapshot.snapshot_dir  = TEST_CASE_DIR / test_name
+    snapshot.assert_match(json.dumps(data, indent=4), 'expected_test_data.json')
